52977048709ef751e26d2670bb366a5536aafcb2
masoudes72 <m.hashemipour72@gmail.com>
2025-07-31 00:47:53 +0330

    Update final-signal.py

--- final-signal.py
+++ final-signal.py
@@ -3,27 +3,28 @@ import pandas as pd
 df = pd.read_csv("btc_15m_with_indicators.csv")
 
 signals = []
+in_position = False
 
 for i in range(1, len(df)):
-    ema_9_prev, ema_21_prev = df['ema_9'].iloc[i-1], df['ema_21'].iloc[i-1]
-    ema_9, ema_21 = df['ema_9'].iloc[i], df['ema_21'].iloc[i]
-    macd, signal_line = df['macd'].iloc[i], df['signal_line'].iloc[i]
+    ema_9 = df['ema_9'].iloc[i]
+    ema_21 = df['ema_21'].iloc[i]
+    macd = df['macd'].iloc[i]
+    signal_line = df['signal_line'].iloc[i]
     rsi = df['rsi_14'].iloc[i]
 
-    # ورود به معامله
-    if (ema_9_prev < ema_21_prev) and (ema_9 > ema_21) and (macd > signal_line) and (rsi < 65):
+    if not in_position and (ema_9 > ema_21 and macd > signal_line and 40 < rsi < 60):
         signals.append("buy")
+        in_position = True
 
-    # خروج از معامله
-    elif (ema_9_prev > ema_21_prev) and (ema_9 < ema_21) and (macd < signal_line) and (rsi > 35):
+    elif in_position and (ema_9 < ema_21 or macd < signal_line):
         signals.append("sell")
+        in_position = False
 
     else:
         signals.append("hold")
 
 df = df.iloc[1:].copy()
 df['signal'] = signals
-
 df.to_csv("btc_signals_15m.csv", index=False)
 
 print("تعداد سیگنال‌ها:")
